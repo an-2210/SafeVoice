b2a834539b2a2856ecccc03ec9e2740dccbb73de
Piyush Yadav <piyushydv011@gmail.com>
2025-06-26 22:23:53 +0530

    chnages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+// Import the Firebase configuration to ensure it's initialized before rendering
+import './lib/firebase';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
